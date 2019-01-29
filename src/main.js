d408bb2765b5bcd8e95d3198b6f5b1812d8ec0d2
Andrey <a.drapatiy@rubarbs.com>
2019-01-29 17:37:20 +0200

    router add

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import VueRouter from 'vue-router'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Main from './components/Main'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+Vue.component('app-sidebar', Sidebar);
+Vue.component('app-header', Header);
+Vue.component('app-main', Main);
+
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
